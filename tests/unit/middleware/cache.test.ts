8d89a31d7afdb26840c56304b1df5abb8c2dfd9a
aryan249 <aryan.soni249@gmail.com>
2022-11-13 15:17:11 +0530

    test: add cache hit and miss tests

--- tests/unit/middleware/cache.test.ts
+++ tests/unit/middleware/cache.test.ts
@@ -1,5 +1,40 @@
+import { Request, Response, NextFunction } from 'express';
+
+const mockGet = jest.fn();
+const mockSetex = jest.fn();
+const mockScan = jest.fn();
+const mockDel = jest.fn();
+
+jest.mock('../../../src/config/redis', () => ({
+  getRedisClient: () => ({
+    get: mockGet,
+    setex: mockSetex,
+    scan: mockScan,
+    del: mockDel,
+  }),
+}));
+
+jest.mock('../../../src/config/index', () => ({
+  default: {
+    server: { env: 'test', port: 3000 },
+    cache: { ttlSeconds: 60 },
+  },
+  __esModule: true,
+}));
+
+jest.mock('../../../src/config/logger', () => ({
+  default: { info: jest.fn(), error: jest.fn(), warn: jest.fn(), debug: jest.fn() },
+  __esModule: true,
+}));
+
+import { cacheMiddleware, invalidateCache } from '../../../src/middleware/cache';
+
 describe('Cache Middleware', () => {
-    it('should return cached response', () => {
-        expect(true).toBe(true);
-    });
-});
+  let mockReq: Partial<Request>;
+  let mockRes: Partial<Response>;
+  let mockNext: NextFunction;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockReq = { method: 'GET', path: '/orders', query: {} };
+    mockRes = {
