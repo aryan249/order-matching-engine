88c1a58490f5904210183d46639ff7ced5c64773
aryan249 <aryan.soni249@gmail.com>
2022-11-14 03:24:22 +0530

    test: add WebSocket subscription tests

--- tests/integration/websocket.test.ts
+++ tests/integration/websocket.test.ts
@@ -1,5 +1,40 @@
-describe('WebSocket Integration', () => {
-    it('should establish connection', () => {
-        expect(true).toBe(true);
+import WebSocket from 'ws';
+
+process.env.JWT_SECRET = 'test-secret';
+process.env.JWT_EXPIRY = '1h';
+
+import { WsServer } from '../../src/websocket/wsServer';
+import { generateToken } from '../../src/utils/jwt';
+import { WsMessageType } from '../../src/types/ws';
+
+jest.mock('../../src/config/logger', () => ({
+  default: { info: jest.fn(), error: jest.fn(), debug: jest.fn() },
+  __esModule: true,
+}));
+
+jest.mock('../../src/metrics/prometheus', () => ({
+  metrics: {
+    wsConnections: { inc: jest.fn(), dec: jest.fn() },
+  },
+}));
+
+describe('WebSocket Server', () => {
+  let wsServer: WsServer;
+  const testPort = 9876;
+
+  beforeAll(() => {
+    wsServer = new WsServer(testPort);
+  });
+
+  afterAll(async () => {
+    await wsServer.close();
+  });
+
+  function connectClient(): Promise<WebSocket> {
+    return new Promise((resolve, reject) => {
+      const ws = new WebSocket(`ws://localhost:${testPort}`);
+      ws.on('open', () => resolve(ws));
+      ws.on('error', reject);
     });
-});
+  }
+
