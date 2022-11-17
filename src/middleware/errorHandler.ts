afce584b6d5ef9f665262b346af09e4bf2e2f9d7
aryan249 <aryan.soni249@gmail.com>
2022-11-17 08:48:53 +0530

    feat: add structured error logging and request context

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -33,3 +33,28 @@ export class ValidationError extends AppError {
     this.name = 'ValidationError';
   }
 }
+
+export function errorHandler(err: Error, _req: Request, res: Response, _next: NextFunction): void {
+  logger.error('Unhandled error', {
+    name: err.name,
+    message: err.message,
+    stack: config.server.env === 'development' ? err.stack : undefined,
+  });
+
+  if (err instanceof AppError) {
+    const response: ApiResponse = {
+      success: false,
+      error: err.message,
+      timestamp: new Date().toISOString(),
+    };
+    res.status(err.statusCode).json(response);
+    return;
+  }
+
+  const response: ApiResponse = {
+    success: false,
+    error: config.server.env === 'production' ? 'Internal server error' : err.message,
+    timestamp: new Date().toISOString(),
+  };
+  res.status(500).json(response);
+}
