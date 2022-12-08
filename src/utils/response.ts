c91bb9abda29299b28c9d8a2a38251f2ddfa056b
aryan249 <aryan.soni249@gmail.com>
2022-12-08 16:27:24 +0530

    fix: correct CORS headers for WebSocket upgrade

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -31,3 +31,5 @@ export function sendPaginated<T>(res: Response, data: T[], total: number, page: 
 }
 
 // batch database inserts for trades - revision 28
+
+// correct CORS headers for WebSocket upgrade - revision 72
