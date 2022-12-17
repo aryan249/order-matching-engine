1b296f1fd32d8d5c1f1b6822b3ba9d5f8e658819
aryan249 <aryan.soni249@gmail.com>
2022-12-17 10:22:12 +0530

    feat: add Winston logger with structured formatting

---
+++ src/config/logger.ts
@@ -0,0 +1,28 @@
+import winston from 'winston';
+import config from './index';
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
+  ),
+  defaultMeta: { service: 'order-matching-engine' },
+  transports: [
+    new winston.transports.Console({
+      format:
+        config.server.env === 'development'
+          ? winston.format.combine(winston.format.colorize(), winston.format.simple())
+          : winston.format.json(),
+    }),
+  ],
+});
+
+if (config.server.env !== 'test') {
+  logger.add(
+    new winston.transports.File({ filename: 'logs/app.log', maxsize: 10_000_000, maxFiles: 5 })
+  );
+}
+
+export default logger;
