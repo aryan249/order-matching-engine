71647b6e8fb85a9062ce11b72f3293c8579f5fc7
aryan249 <aryan.soni249@gmail.com>
2022-12-29 12:40:16 +0530

    feat: add POST endpoint for order creation

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -1,9 +1,14 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req: Request, res: Response) => {
     res.json({ orders: [] });
+});
+
+router.post('/', async (req: Request, res: Response) => {
+    const { asset, side, type, price, quantity } = req.body;
+    res.status(201).json({ message: 'Order created' });
 });
 
 export default router;
