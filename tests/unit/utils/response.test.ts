9723f20476397a7625b0a67e0567881d21d42fbe
aryan249 <aryan.soni249@gmail.com>
2023-01-05 11:58:02 +0530

    test: add response helper utility unit tests

--- tests/unit/utils/response.test.ts
+++ tests/unit/utils/response.test.ts
@@ -1,5 +1,44 @@
+import { Response } from 'express';
+import { sendSuccess, sendError, sendPaginated } from '../../../src/utils/response';
+
 describe('Response Helpers', () => {
-    it('should create success response', () => {
-        expect(true).toBe(true);
-    });
+  let mockRes: Partial<Response>;
+
+  beforeEach(() => {
+    mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+  });
+
+  it('sendSuccess should return 200 with data', () => {
+    sendSuccess(mockRes as Response, { id: '1' });
+
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+    expect(mockRes.json).toHaveBeenCalledWith(
+      expect.objectContaining({ success: true, data: { id: '1' } })
+    );
+  });
+
+  it('sendSuccess should support custom status code', () => {
+    sendSuccess(mockRes as Response, { id: '1' }, 201);
+    expect(mockRes.status).toHaveBeenCalledWith(201);
+  });
+
+  it('sendError should return error response', () => {
+    sendError(mockRes as Response, 'Something went wrong', 500);
+
+    expect(mockRes.status).toHaveBeenCalledWith(500);
+    expect(mockRes.json).toHaveBeenCalledWith(
+      expect.objectContaining({ success: false, error: 'Something went wrong' })
+    );
+  });
+
+  it('sendPaginated should include pagination fields', () => {
+    sendPaginated(mockRes as Response, [{ id: '1' }], 50, 2, 10);
+
+    expect(mockRes.json).toHaveBeenCalledWith(
+      expect.objectContaining({ success: true, data: [{ id: '1' }], total: 50, page: 2, limit: 10 })
+    );
+  });
 });
