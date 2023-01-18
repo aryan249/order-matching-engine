e37d91f18b17311476e202731ca9b38f887c38a9
aryan249 <aryan.soni249@gmail.com>
2023-01-18 15:44:03 +0530

    feat: add registration endpoint to auth routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,45 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
+import bcrypt from 'bcrypt';
+import { z } from 'zod';
+import { query } from '../config/database';
+import { generateToken } from '../utils/jwt';
+import { validate } from '../middleware/validate';
 
 const router = Router();
 
-router.post('/login', async (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    res.json({ token: 'stub' });
+const AuthSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8).max(128),
 });
 
-export default router;
+function asyncWrap(fn: (req: Request, res: Response, next: NextFunction) => Promise<void>) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res, next).catch(next);
+  };
+}
+
+router.post('/register', validate(AuthSchema), asyncWrap(async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const existing = await query('SELECT id FROM users WHERE email = $1', [email]);
+  if (existing.rows.length > 0) {
+    res.status(409).json({
+      success: false,
+      error: 'Email already registered',
+      timestamp: new Date().toISOString(),
+    });
+    return;
+  }
+
+  const passwordHash = await bcrypt.hash(password, 12);
+  const result = await query(
+    'INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id, email',
+    [email, passwordHash]
+  );
+
+  const user = result.rows[0];
+  const token = generateToken({ userId: user.id, email: user.email });
+
+  res.status(201).json({
+    success: true,
+    data: { token, userId: user.id, email: user.email },
