07c7b52054155ad24216576776883753609afab5
aryan249 <aryan.soni249@gmail.com>
2023-02-06 06:50:21 +0530

    feat: add Redis connection event handlers and retry logic

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,5 +1,39 @@
 import Redis from 'ioredis';
-import { config } from './index';
+import config from './index';
+import logger from './logger';
 
-const redis = new Redis(config.redisUrl);
-export default redis;
+let client: Redis | null = null;
+
+export function getRedisClient(): Redis {
+  if (!client) {
+    client = new Redis(config.redis.url, {
+      maxRetriesPerRequest: 3,
+      retryStrategy(times) {
+        const delay = Math.min(times * 200, 2000);
+        return delay;
+      },
+    });
+
+    client.on('connect', () => logger.info('Redis connected'));
+    client.on('error', (err) => logger.error('Redis error', { error: err.message }));
+  }
+  return client;
+}
+
+export function createRedisClient(): Redis {
+  const newClient = new Redis(config.redis.url, {
+    maxRetriesPerRequest: 3,
+    retryStrategy(times) {
+      const delay = Math.min(times * 200, 2000);
+      return delay;
+    },
+  });
+  return newClient;
+}
+
+export async function disconnectRedis(): Promise<void> {
+  if (client) {
+    await client.quit();
+    client = null;
+  }
+}
