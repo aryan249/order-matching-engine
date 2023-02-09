6b6b3f5f89ff4f2f50e9c7deaf95de37e0daa51f
aryan249 <aryan.soni249@gmail.com>
2023-02-10 01:23:10 +0530

    feat: add dequeue and peek operations to Redis queue

--- src/queue/redisQueue.ts
+++ src/queue/redisQueue.ts
@@ -1,13 +1,40 @@
-import redis from '../config/redis';
+import { getRedisClient } from '../config/redis';
+import { Order } from '../types/order';
+import logger from '../config/logger';
 
 export class RedisQueue {
-    private queueName: string;
+  private keyPrefix = 'queue:orders:';
+  private assetsKey = 'queue:assets';
 
-    constructor(queueName: string) {
-        this.queueName = queueName;
+  private getKey(asset: string): string {
+    return `${this.keyPrefix}${asset}`;
+  }
+
+  async enqueue(asset: string, order: Order): Promise<void> {
+    const redis = getRedisClient();
+    const pipeline = redis.pipeline();
+    pipeline.rpush(this.getKey(asset), JSON.stringify(order));
+    pipeline.sadd(this.assetsKey, asset);
+    await pipeline.exec();
+    logger.debug('Order enqueued', { orderId: order.id, asset });
+  }
+
+  async dequeue(asset: string, count: number): Promise<Order[]> {
+    const redis = getRedisClient();
+    const orders: Order[] = [];
+
+    for (let i = 0; i < count; i++) {
+      const data = await redis.lpop(this.getKey(asset));
+      if (!data) break;
+      orders.push(JSON.parse(data));
     }
 
-    async enqueue(data: any): Promise<void> {
-        await redis.lpush(this.queueName, JSON.stringify(data));
-    }
-}
+    return orders;
+  }
+
+  async peek(asset: string, count: number): Promise<Order[]> {
+    const redis = getRedisClient();
+    const items = await redis.lrange(this.getKey(asset), 0, count - 1);
+    return items.map((item) => JSON.parse(item));
+  }
+
