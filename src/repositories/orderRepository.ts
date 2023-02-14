6fe2d1827aa11ab5ec2f11a8afa339fbc649b7b1
aryan249 <aryan.soni249@gmail.com>
2023-02-14 21:26:47 +0530

    feat: add findById and findAll to order repository

--- src/repositories/orderRepository.ts
+++ src/repositories/orderRepository.ts
@@ -1,12 +1,50 @@
-import pool from '../config/database';
-import { Order } from '../types/order';
+import { query, getClient } from '../config/database';
+import { Order, OrderStatus, CreateOrderDto } from '../types/order';
+
+function rowToOrder(row: Record<string, unknown>): Order {
+  return {
+    id: row.id as string,
+    userId: row.user_id as string,
+    asset: row.asset as string,
+    side: row.side as Order['side'],
+    price: parseFloat(row.price as string),
+    quantity: parseFloat(row.quantity as string),
+    remainingQuantity: parseFloat(row.remaining_quantity as string),
+    status: row.status as OrderStatus,
+    createdAt: new Date(row.created_at as string),
+    updatedAt: new Date(row.updated_at as string),
+  };
+}
 
 export class OrderRepository {
-    async create(order: Order): Promise<Order> {
-        const result = await pool.query(
-            'INSERT INTO orders (id, asset, side, type, price, quantity, status) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
-            [order.id, order.asset, order.side, order.type, order.price, order.quantity, order.status]
-        );
-        return result.rows[0];
-    }
-}
+  async create(dto: CreateOrderDto & { userId: string }): Promise<Order> {
+    const result = await query(
+      `INSERT INTO orders (user_id, asset, side, price, quantity, remaining_quantity, status)
+       VALUES ($1, $2, $3, $4, $5, $5, 'pending')
+       RETURNING *`,
+      [dto.userId, dto.asset, dto.side, dto.price, dto.quantity]
+    );
+    return rowToOrder(result.rows[0]);
+  }
+
+  async findById(id: string): Promise<Order | null> {
+    const result = await query('SELECT * FROM orders WHERE id = $1', [id]);
+    return result.rows.length > 0 ? rowToOrder(result.rows[0]) : null;
+  }
+
+  async findByAssetAndStatus(asset: string, status: OrderStatus): Promise<Order[]> {
+    const result = await query(
+      'SELECT * FROM orders WHERE asset = $1 AND status = $2 ORDER BY price ASC, created_at ASC',
+      [asset, status]
+    );
+    return result.rows.map(rowToOrder);
+  }
+
+  async updateStatus(id: string, status: OrderStatus, remainingQuantity?: number): Promise<Order> {
+    const client = await getClient();
+    try {
+      const fields = ['status = $2', 'updated_at = NOW()'];
+      const params: unknown[] = [id, status];
+
+      if (remainingQuantity !== undefined) {
+        fields.push(`remaining_quantity = $${params.length + 1}`);
