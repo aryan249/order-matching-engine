4072cbe1c14b8ff50447177e84492e6322ff494b
aryan249 <aryan.soni249@gmail.com>
2023-02-23 20:20:25 +0530

    feat: add message parsing and channel subscriptions

--- src/websocket/wsServer.ts
+++ src/websocket/wsServer.ts
@@ -1,17 +1,60 @@
 import { WebSocketServer, WebSocket } from 'ws';
-import http from 'http';
+import { IncomingMessage } from 'http';
+import { verifyToken } from '../utils/jwt';
+import { WsMessage, WsMessageType } from '../types/ws';
+import { metrics } from '../metrics/prometheus';
+import logger from '../config/logger';
+
+interface AuthenticatedSocket extends WebSocket {
+  userId?: string;
+  isAlive?: boolean;
+}
 
 export class WsServer {
-    private wss: WebSocketServer;
+  private wss: WebSocketServer;
+  private userConnections: Map<string, Set<AuthenticatedSocket>> = new Map();
+  private assetSubscriptions: Map<string, Set<string>> = new Map();
+  private heartbeatInterval: NodeJS.Timeout;
 
-    constructor(server: http.Server) {
-        this.wss = new WebSocketServer({ server });
-    }
+  constructor(port: number) {
+    this.wss = new WebSocketServer({ port });
 
-    start(): void {
-        this.wss.on('connection', (ws: WebSocket) => {
-            console.log('Client connected');
-            ws.on('close', () => console.log('Client disconnected'));
-        });
-    }
-}
+    this.wss.on('connection', (ws: AuthenticatedSocket, _req: IncomingMessage) => {
+      ws.isAlive = true;
+      metrics.wsConnections.inc();
+
+      const authTimeout = setTimeout(() => {
+        if (!ws.userId) {
+          ws.close(4001, 'Authentication timeout');
+        }
+      }, 5000);
+
+      ws.on('message', (data: Buffer) => {
+        try {
+          const message = JSON.parse(data.toString());
+          this.handleMessage(ws, message, authTimeout);
+        } catch {
+          this.sendError(ws, 'Invalid message format');
+        }
+      });
+
+      ws.on('pong', () => {
+        ws.isAlive = true;
+      });
+
+      ws.on('close', () => {
+        clearTimeout(authTimeout);
+        this.removeConnection(ws);
+        metrics.wsConnections.dec();
+      });
+    });
+
+    this.heartbeatInterval = setInterval(() => {
+      this.wss.clients.forEach((ws) => {
+        const socket = ws as AuthenticatedSocket;
+        if (!socket.isAlive) {
+          socket.terminate();
+          return;
+        }
+        socket.isAlive = false;
+        socket.ping();
