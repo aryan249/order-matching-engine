121dd536af9886ff5a94263872fd94e704af5cd0
aryan249 <aryan.soni249@gmail.com>
2023-03-05 13:35:40 +0530

    test: add basic order matching integration tests

--- tests/integration/matching.test.ts
+++ tests/integration/matching.test.ts
@@ -1,5 +1,35 @@
-describe('Matching Engine Integration', () => {
-    it('should match buy and sell orders', () => {
-        expect(true).toBe(true);
-    });
-});
+import { OrderSide, OrderStatus, Order } from '../../src/types/order';
+
+const mockPeek = jest.fn();
+const mockRemoveOrder = jest.fn();
+const mockUpdateOrder = jest.fn();
+const mockUpdateStatus = jest.fn();
+const mockPublish = jest.fn();
+
+jest.mock('../../src/config/redis', () => ({
+  getRedisClient: () => ({}),
+  createRedisClient: () => ({
+    on: jest.fn(),
+    subscribe: jest.fn(),
+    publish: jest.fn(),
+    quit: jest.fn(),
+  }),
+}));
+
+jest.mock('../../src/metrics/prometheus', () => ({
+  metrics: {
+    ordersMatched: { inc: jest.fn() },
+    matchingDuration: { startTimer: () => jest.fn() },
+  },
+}));
+
+jest.mock('../../src/config/logger', () => ({
+  default: { info: jest.fn(), error: jest.fn(), debug: jest.fn() },
+  __esModule: true,
+}));
+
+import { OrderMatchingService } from '../../src/services/orderMatching';
+
+describe('Order Matching Integration', () => {
+  let service: OrderMatchingService;
+
