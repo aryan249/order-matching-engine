9fdffa9b334a7802e6c4fe4eee4b680f9b63222c
aryan249 <aryan.soni249@gmail.com>
2023-03-10 06:35:21 +0530

    feat: implement Redis-based sliding window rate limiter

--- src/middleware/rateLimit.ts
+++ src/middleware/rateLimit.ts
@@ -1,15 +1,46 @@
 import { Request, Response, NextFunction } from 'express';
+import { getRedisClient } from '../config/redis';
+import config from '../config/index';
 
-const requestCounts = new Map<string, number>();
+interface RateLimitOptions {
+  windowMs?: number;
+  maxRequests?: number;
+}
 
-export function rateLimit(maxRequests: number = 100) {
-    return (req: Request, res: Response, next: NextFunction) => {
-        const ip = req.ip || 'unknown';
-        const count = requestCounts.get(ip) || 0;
-        if (count >= maxRequests) {
-            return res.status(429).json({ error: 'Too many requests' });
-        }
-        requestCounts.set(ip, count + 1);
-        next();
-    };
+export function rateLimitMiddleware(options: RateLimitOptions = {}) {
+  const windowMs = options.windowMs || config.rateLimit.windowMs;
+  const maxRequests = options.maxRequests || config.rateLimit.maxRequests;
+  const windowSeconds = Math.ceil(windowMs / 1000);
+
+  return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const userId = req.user?.userId || req.ip;
+    const key = `ratelimit:${userId}`;
+
+    try {
+      const redis = getRedisClient();
+      const current = await redis.incr(key);
+
+      if (current === 1) {
+        await redis.expire(key, windowSeconds);
+      }
+
+      res.setHeader('X-RateLimit-Limit', maxRequests);
+      res.setHeader('X-RateLimit-Remaining', Math.max(0, maxRequests - current));
+
+      if (current > maxRequests) {
+        const ttl = await redis.ttl(key);
+        res.setHeader('Retry-After', ttl);
+        res.status(429).json({
+          success: false,
+          error: 'Too many requests',
+          timestamp: new Date().toISOString(),
+        });
+        return;
+      }
+
+      next();
+    } catch {
+      next();
+    }
+  };
 }
