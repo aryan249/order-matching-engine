a50f776355812fa56ab79ba02cdf160ca49b4c8c
aryan249 <aryan.soni249@gmail.com>
2023-03-21 16:51:46 +0530

    feat: add Redis-backed response caching logic

--- src/middleware/cache.ts
+++ src/middleware/cache.ts
@@ -1,8 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
+import crypto from 'crypto';
+import { RedisCache } from '../cache/redisCache';
 
-export function cacheMiddleware(ttl: number = 60) {
-    return (req: Request, res: Response, next: NextFunction) => {
-        // Cache logic
-        next();
-    };
+const cache = new RedisCache();
+
+interface CacheOptions {
+  ttl?: number;
 }
+
+export function cacheMiddleware(options: CacheOptions = {}) {
+  return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    if (req.method !== 'GET') {
+      next();
+      return;
+    }
+
+    const queryHash = crypto.createHash('md5').update(JSON.stringify(req.query)).digest('hex').substring(0, 8);
+
+    try {
+      const cached = await cache.get(req.method, req.path, queryHash);
+      if (cached) {
+        res.setHeader('X-Cache', 'HIT');
+        res.json(JSON.parse(cached));
+        return;
