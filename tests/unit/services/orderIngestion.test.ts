484346b173bbc148a83d6aa0b4683263fc76b8ae
aryan249 <aryan.soni249@gmail.com>
2023-03-23 03:23:48 +0530

    test: add input validation tests for order ingestion

--- tests/unit/services/orderIngestion.test.ts
+++ tests/unit/services/orderIngestion.test.ts
@@ -1,5 +1,35 @@
+import { OrderSide, OrderStatus, Order, CreateOrderDto } from '../../../src/types/order';
+
+const mockCreate = jest.fn();
+const mockEnqueue = jest.fn();
+const mockPublish = jest.fn();
+
+jest.mock('../../../src/metrics/prometheus', () => ({
+  metrics: {
+    ordersSubmitted: { inc: jest.fn() },
+  },
+}));
+
+jest.mock('../../../src/config/logger', () => ({
+  default: { info: jest.fn(), error: jest.fn(), debug: jest.fn() },
+  __esModule: true,
+}));
+
+import { OrderIngestionService } from '../../../src/services/orderIngestion';
+
 describe('OrderIngestionService', () => {
-    it('should validate order input', () => {
-        expect(true).toBe(true);
-    });
-});
+  let service: OrderIngestionService;
+  const mockOrderRepo = { create: mockCreate } as any;
+  const mockQueue = { enqueue: mockEnqueue } as any;
+  const mockPubsub = { publish: mockPublish } as any;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    service = new OrderIngestionService(mockOrderRepo, mockQueue, mockPubsub);
+  });
+
+  const makeMockOrder = (side: OrderSide): Order => ({
+    id: 'order-1',
+    userId: 'user-1',
+    asset: 'BTC',
+    side,
