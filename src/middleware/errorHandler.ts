d9c9d06f5bf12be0c1d94e9c5fb7fbc7a08bbae6
aryan249 <aryan.soni249@gmail.com>
2023-04-26 00:02:29 +0530

    feat: add error classification and status codes

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,6 +1,35 @@
 import { Request, Response, NextFunction } from 'express';
+import { ApiResponse } from '../types/api';
+import logger from '../config/logger';
+import config from '../config/index';
 
-export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-    console.error(err.stack);
-    res.status(500).json({ error: 'Internal Server Error' });
+export class AppError extends Error {
+  constructor(
+    public statusCode: number,
+    message: string
+  ) {
+    super(message);
+    this.name = 'AppError';
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message = 'Resource not found') {
+    super(404, message);
+    this.name = 'NotFoundError';
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message = 'Unauthorized') {
+    super(401, message);
+    this.name = 'UnauthorizedError';
+  }
+}
+
+export class ValidationError extends AppError {
+  constructor(message: string) {
+    super(400, message);
+    this.name = 'ValidationError';
+  }
 }
