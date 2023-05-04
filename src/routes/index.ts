25d90f0de0c59a5dae6ef187376403b6823d3ebf
aryan249 <aryan.soni249@gmail.com>
2023-05-04 15:46:50 +0530

    feat: wire auth and metrics routes into aggregator

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,38 @@
-import { Router } from 'express';
-import orderRoutes from './orders';
+import { Router, Request, Response } from 'express';
+import { getRedisClient } from '../config/redis';
+import { pool } from '../config/database';
+import authRoutes from './auth';
+import metricsRoutes from './metrics';
 
-const router = Router();
-router.use('/orders', orderRoutes);
+export function createRouter(orderRoutes: Router): Router {
+  const router = Router();
 
-export default router;
+  router.get('/health', async (_req: Request, res: Response) => {
+    const redisOk = getRedisClient().status === 'ready';
+    let dbOk = false;
+    try {
+      await pool.query('SELECT 1');
+      dbOk = true;
+    } catch {
+      // db not connected
+    }
+
+    const status = redisOk && dbOk ? 'ok' : 'degraded';
+    const statusCode = status === 'ok' ? 200 : 503;
+
+    res.status(statusCode).json({
+      status,
+      uptime: process.uptime(),
+      redis: redisOk ? 'connected' : 'disconnected',
+      database: dbOk ? 'connected' : 'disconnected',
+      timestamp: new Date().toISOString(),
+      version: process.env.npm_package_version || '1.0.0',
+    });
+  });
+
+  router.use('/auth', authRoutes);
+  router.use('/orders', orderRoutes);
+  router.use('/metrics', metricsRoutes);
+
+  return router;
+}
