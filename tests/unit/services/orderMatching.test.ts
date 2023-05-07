ffb7e07f7bf1bfc82c4a498457fe5f0e63156855
aryan249 <aryan.soni249@gmail.com>
2023-05-07 11:15:28 +0530

    test: add basic order matching assertions

--- tests/unit/services/orderMatching.test.ts
+++ tests/unit/services/orderMatching.test.ts
@@ -1,5 +1,30 @@
+import { OrderSide, OrderStatus, Order } from '../../../src/types/order';
+
+const mockPeek = jest.fn();
+const mockRemoveOrder = jest.fn();
+const mockUpdateOrder = jest.fn();
+const mockPublish = jest.fn();
+const mockUpdateStatus = jest.fn();
+
+jest.mock('../../../src/metrics/prometheus', () => ({
+  metrics: {
+    ordersMatched: { inc: jest.fn() },
+    matchingDuration: { startTimer: () => jest.fn() },
+  },
+}));
+
+jest.mock('../../../src/config/logger', () => ({
+  default: { info: jest.fn(), error: jest.fn(), debug: jest.fn() },
+  __esModule: true,
+}));
+
+import { OrderMatchingService } from '../../../src/services/orderMatching';
+
 describe('OrderMatchingService', () => {
-    it('should exist', () => {
-        expect(true).toBe(true);
-    });
-});
+  let service: OrderMatchingService;
+
+  const mockQueue = {
+    peek: mockPeek,
+    removeOrder: mockRemoveOrder,
+    updateOrder: mockUpdateOrder,
+  } as any;
