4aa7aac82ef52bee1d3c2be14abf2a629f94d28b
aryan249 <aryan.soni249@gmail.com>
2023-06-11 22:03:38 +0530

    refactor: extract validation error formatter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -40,3 +40,5 @@ export function createRouter(orderRoutes: Router): Router {
 // optimize WebSocket message serialization - revision 23
 
 // prevent stale cache reads after order update - revision 67
+
+// extract validation error formatter - revision 111
