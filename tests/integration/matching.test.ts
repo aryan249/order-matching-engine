7e04890410705318a74b08e8661cef4642cbbb32
aryan249 <aryan.soni249@gmail.com>
2023-07-07 17:36:50 +0530

    test: add partial fill integration tests

--- tests/integration/matching.test.ts
+++ tests/integration/matching.test.ts
@@ -33,3 +33,33 @@ import { OrderMatchingService } from '../../src/services/orderMatching';
 describe('Order Matching Integration', () => {
   let service: OrderMatchingService;
 
+  beforeEach(() => {
+    jest.clearAllMocks();
+    service = new OrderMatchingService(
+      { peek: mockPeek, removeOrder: mockRemoveOrder, updateOrder: mockUpdateOrder } as any,
+      { publish: mockPublish, subscribe: jest.fn() } as any,
+      { updateStatus: mockUpdateStatus } as any
+    );
+  });
+
+  it('should execute a full matching cycle: maker queued, taker matches', async () => {
+    const maker: Order = {
+      id: 'maker-1',
+      userId: 'seller-1',
+      asset: 'ETH',
+      side: OrderSide.MAKER,
+      price: 3000,
+      quantity: 5,
+      remainingQuantity: 5,
+      status: OrderStatus.PENDING,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    const taker: Order = {
+      id: 'taker-1',
+      userId: 'buyer-1',
+      asset: 'ETH',
+      side: OrderSide.TAKER,
+      price: 3100,
+      quantity: 3,
