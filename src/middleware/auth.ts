e4142f136f0b0e63a20fe8daf7455ee61511251f
aryan249 <aryan.soni249@gmail.com>
2023-07-08 23:02:57 +0530

    feat: add JWT verification to auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,9 +1,30 @@
 import { Request, Response, NextFunction } from 'express';
+import { verifyToken } from '../utils/jwt';
+import { AuthPayload } from '../types/api';
+import { UnauthorizedError } from './errorHandler';
 
-export function authMiddleware(req: Request, res: Response, next: NextFunction) {
-    const token = req.headers.authorization?.split(' ')[1];
-    if (!token) {
-        return res.status(401).json({ error: 'Unauthorized' });
+declare global {
+  namespace Express {
+    interface Request {
+      user?: AuthPayload;
     }
+  }
+}
+
+export function authMiddleware(req: Request, _res: Response, next: NextFunction): void {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    next(new UnauthorizedError('Missing or invalid authorization header'));
+    return;
+  }
+
+  const token = authHeader.substring(7);
+
+  try {
+    req.user = verifyToken(token);
     next();
+  } catch {
+    next(new UnauthorizedError('Invalid or expired token'));
+  }
 }
