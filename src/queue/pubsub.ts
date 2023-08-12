2d885b3eca448e4eda700e1dd520c09ba3a060f8
aryan249 <aryan.soni249@gmail.com>
2023-08-12 19:39:38 +0530

    feat: add publish and subscribe methods to pub/sub

--- src/queue/pubsub.ts
+++ src/queue/pubsub.ts
@@ -1,12 +1,35 @@
+import { createRedisClient } from '../config/redis';
 import Redis from 'ioredis';
-import { config } from '../config';
+import logger from '../config/logger';
 
-export class PubSub {
-    private publisher: Redis;
-    private subscriber: Redis;
+export const Channels = {
+  ORDER_NEW: 'channel:order:new',
+  ORDER_MATCHED: 'channel:order:matched',
+  TRADE_EXECUTED: 'channel:trade:executed',
+  BATCH_COMPLETE: 'channel:batch:complete',
+} as const;
 
-    constructor() {
-        this.publisher = new Redis(config.redisUrl);
-        this.subscriber = new Redis(config.redisUrl);
-    }
-}
+type MessageHandler = (message: unknown) => void;
+
+export class RedisPubSub {
+  private pub: Redis;
+  private sub: Redis;
+  private handlers: Map<string, MessageHandler[]> = new Map();
+
+  constructor() {
+    this.pub = createRedisClient();
+    this.sub = createRedisClient();
+
+    this.sub.on('message', (channel: string, message: string) => {
+      const channelHandlers = this.handlers.get(channel);
+      if (channelHandlers) {
+        try {
+          const parsed = JSON.parse(message);
+          channelHandlers.forEach((handler) => handler(parsed));
+        } catch (err) {
+          logger.error('Failed to parse pubsub message', { channel, error: (err as Error).message });
+        }
+      }
+    });
+  }
+
