7fc9cf0ae1b3ac7807e7a15b8c670ad128cd9e19
aryan249 <aryan.soni249@gmail.com>
2023-08-13 18:37:06 +0530

    test: add rate limit threshold tests

--- tests/unit/middleware/rateLimit.test.ts
+++ tests/unit/middleware/rateLimit.test.ts
@@ -1,5 +1,35 @@
+import { Request, Response, NextFunction } from 'express';
+
+const mockIncr = jest.fn();
+const mockExpire = jest.fn();
+const mockTtl = jest.fn();
+
+jest.mock('../../../src/config/redis', () => ({
+  getRedisClient: () => ({
+    incr: mockIncr,
+    expire: mockExpire,
+    ttl: mockTtl,
+  }),
+}));
+
+jest.mock('../../../src/config/index', () => ({
+  default: {
+    rateLimit: { windowMs: 60000, maxRequests: 5 },
+  },
+  __esModule: true,
+}));
+
+import { rateLimitMiddleware } from '../../../src/middleware/rateLimit';
+
 describe('Rate Limit Middleware', () => {
-    it('should allow requests under limit', () => {
-        expect(true).toBe(true);
-    });
-});
+  let mockReq: Partial<Request>;
+  let mockRes: Partial<Response>;
+  let mockNext: NextFunction;
+  const middleware = rateLimitMiddleware({ maxRequests: 5 });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockReq = { user: { userId: 'user-1', email: 'test@test.com' }, ip: '127.0.0.1' };
+    mockRes = {
+      setHeader: jest.fn(),
+      status: jest.fn().mockReturnThis(),
