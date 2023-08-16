a74aeca5dbef40db0a53eef0226806b30dd49fc0
aryan249 <aryan.soni249@gmail.com>
2023-08-16 10:25:13 +0530

    feat: add trading constants for supported assets and limits

---
+++ src/types/constants.ts
@@ -0,0 +1,6 @@
+export const SUPPORTED_ASSETS = ['BTC', 'ETH', 'SOL', 'AVAX', 'MATIC', 'DOT', 'ADA', 'LINK'] as const;
+
+export const MAX_ORDER_PRICE = 1_000_000_000;
+export const MIN_ORDER_QUANTITY = 0.00000001;
+export const MAX_PAGE_SIZE = 100;
+export const DEFAULT_PAGE_SIZE = 20;
