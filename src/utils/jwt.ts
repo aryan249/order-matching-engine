9c321ee831b12c73df385ecfb27c714d32da3075
aryan249 <aryan.soni249@gmail.com>
2023-09-06 10:46:13 +0530

    feat: add JWT sign and verify utility functions

---
+++ src/utils/jwt.ts
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken';
+import type { StringValue } from 'ms';
+import config from '../config/index';
+import { AuthPayload } from '../types/api';
+
+export function generateToken(payload: { userId: string; email: string }): string {
+  return jwt.sign(payload, config.jwt.secret, { expiresIn: config.jwt.expiry as StringValue });
+}
+
+export function verifyToken(token: string): AuthPayload {
+  return jwt.verify(token, config.jwt.secret) as AuthPayload;
+}
