1f79c0fceaeddc796f084001fb154570f3eb2504
aryan249 <aryan.soni249@gmail.com>
2023-09-13 23:18:58 +0530

    feat: add order insertion with price-priority sorting

--- src/services/orderMatching.ts
+++ src/services/orderMatching.ts
@@ -1,6 +1,16 @@
-import { Order, OrderSide } from '../types/order';
+import { Order, OrderSide, OrderType, OrderStatus } from '../types/order';
 
 export class OrderMatchingService {
     private buyOrders: Order[] = [];
     private sellOrders: Order[] = [];
+
+    addOrder(order: Order): void {
+        if (order.side === OrderSide.BUY) {
+            this.buyOrders.push(order);
+            this.buyOrders.sort((a, b) => b.price - a.price);
+        } else {
+            this.sellOrders.push(order);
+            this.sellOrders.sort((a, b) => a.price - b.price);
+        }
+    }
 }
