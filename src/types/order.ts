e02d017e38a3f094b3e0f3459f3cfe5dc7e139f1
aryan249 <aryan.soni249@gmail.com>
2023-09-23 15:06:56 +0530

    feat: add Order and CreateOrderRequest interfaces

--- src/types/order.ts
+++ src/types/order.ts
@@ -1,17 +1,37 @@
-export enum OrderSide {
-    BUY = 'BUY',
-    SELL = 'SELL',
-}
+import { z } from 'zod';
 
-export enum OrderType {
-    LIMIT = 'LIMIT',
-    MARKET = 'MARKET',
+export enum OrderSide {
+  MAKER = 'maker',
+  TAKER = 'taker',
 }
 
 export enum OrderStatus {
-    PENDING = 'PENDING',
-    OPEN = 'OPEN',
-    FILLED = 'FILLED',
-    PARTIALLY_FILLED = 'PARTIALLY_FILLED',
-    CANCELLED = 'CANCELLED',
+  PENDING = 'pending',
+  MATCHED = 'matched',
+  PARTIALLY_MATCHED = 'partially_matched',
+  EXECUTED = 'executed',
+  CANCELLED = 'cancelled',
+  FAILED = 'failed',
 }
+
+export interface Order {
+  id: string;
+  userId: string;
+  asset: string;
+  side: OrderSide;
+  price: number;
+  quantity: number;
+  remainingQuantity: number;
+  status: OrderStatus;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const CreateOrderSchema = z.object({
+  asset: z.string().min(1).max(20).toUpperCase(),
+  side: z.nativeEnum(OrderSide),
+  price: z.number().positive(),
+  quantity: z.number().positive(),
+});
+
+export type CreateOrderDto = z.infer<typeof CreateOrderSchema>;
