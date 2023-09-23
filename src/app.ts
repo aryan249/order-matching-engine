0453cd5557920563a7e1df6aee25f0ebd2cda798
aryan249 <aryan.soni249@gmail.com>
2023-09-23 12:20:44 +0530

    feat: wire middleware and routes into Express app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,31 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import { Router } from 'express';
+import { createRouter } from './routes/index';
+import { metricsMiddleware } from './metrics/prometheus';
+import { requestIdMiddleware } from './middleware/requestId';
+import { errorHandler } from './middleware/errorHandler';
+import logger from './config/logger';
 
-const app = express();
-app.use(express.json());
+export function createApp(orderRoutes: Router): express.Application {
+  const app = express();
 
-export default app;
+  app.use(helmet());
+  app.use(cors());
+  app.use(express.json({ limit: '1mb' }));
+  app.use(requestIdMiddleware);
+  app.use(metricsMiddleware);
+
+  app.use((req, _res, next) => {
+    logger.info('HTTP Request', { method: req.method, path: req.path, ip: req.ip });
+    next();
+  });
+
+  const router = createRouter(orderRoutes);
+  app.use(router);
+
+  app.use(errorHandler);
+
+  return app;
+}
