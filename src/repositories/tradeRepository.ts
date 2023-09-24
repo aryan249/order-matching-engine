26e24be7e47f7e00706422f1605f83146b3b2c92
aryan249 <aryan.soni249@gmail.com>
2023-09-25 04:29:40 +0530

    feat: add findById and recent trades query

--- src/repositories/tradeRepository.ts
+++ src/repositories/tradeRepository.ts
@@ -1,12 +1,40 @@
-import pool from '../config/database';
+import { query } from '../config/database';
 import { Trade } from '../types/trade';
 
-export class TradeRepository {
-    async create(trade: Trade): Promise<Trade> {
-        const result = await pool.query(
-            'INSERT INTO trades (id, buy_order_id, sell_order_id, price, quantity) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-            [trade.id, trade.buyOrderId, trade.sellOrderId, trade.price, trade.quantity]
-        );
-        return result.rows[0];
-    }
+function rowToTrade(row: Record<string, unknown>): Trade {
+  return {
+    id: row.id as string,
+    makerOrderId: row.maker_order_id as string,
+    takerOrderId: row.taker_order_id as string,
+    asset: row.asset as string,
+    price: parseFloat(row.price as string),
+    quantity: parseFloat(row.quantity as string),
+    executedAt: new Date(row.executed_at as string),
+  };
 }
+
+export class TradeRepository {
+  async create(trade: Omit<Trade, 'id' | 'executedAt'>): Promise<Trade> {
+    const result = await query(
+      `INSERT INTO trades (maker_order_id, taker_order_id, asset, price, quantity)
+       VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+      [trade.makerOrderId, trade.takerOrderId, trade.asset, trade.price, trade.quantity]
+    );
+    return rowToTrade(result.rows[0]);
+  }
+
+  async findByOrderId(orderId: string): Promise<Trade[]> {
+    const result = await query(
+      'SELECT * FROM trades WHERE maker_order_id = $1 OR taker_order_id = $1 ORDER BY executed_at DESC',
+      [orderId]
+    );
+    return result.rows.map(rowToTrade);
+  }
+
+  async findByAsset(asset: string, page: number, limit: number): Promise<{ trades: Trade[]; total: number }> {
+    const offset = (page - 1) * limit;
+    const [dataResult, countResult] = await Promise.all([
+      query('SELECT * FROM trades WHERE asset = $1 ORDER BY executed_at DESC LIMIT $2 OFFSET $3', [
+        asset,
+        limit,
+        offset,
