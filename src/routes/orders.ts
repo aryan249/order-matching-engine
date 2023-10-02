92bb60d30f3bf27f52c91de02a05088e52edb368
aryan249 <aryan.soni249@gmail.com>
2023-10-02 12:24:09 +0530

    feat: add GET by ID and DELETE order endpoints

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -1,14 +1,60 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
+import { authMiddleware } from '../middleware/auth';
+import { rateLimitMiddleware } from '../middleware/rateLimit';
+import { cacheMiddleware, invalidateCache } from '../middleware/cache';
+import { validate } from '../middleware/validate';
+import { CreateOrderSchema } from '../types/order';
+import { OrderIngestionService } from '../services/orderIngestion';
+import { OrderRepository } from '../repositories/orderRepository';
+import { TradeService } from '../services/tradeService';
+import { RedisQueue } from '../queue/redisQueue';
+import { NotFoundError } from '../middleware/errorHandler';
 
-const router = Router();
+type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<void>;
 
-router.get('/', async (req: Request, res: Response) => {
-    res.json({ orders: [] });
-});
+function asyncWrap(fn: AsyncHandler) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res, next).catch(next);
+  };
+}
 
-router.post('/', async (req: Request, res: Response) => {
-    const { asset, side, type, price, quantity } = req.body;
-    res.status(201).json({ message: 'Order created' });
-});
+export function createOrderRoutes(
+  orderIngestion: OrderIngestionService,
+  orderRepo: OrderRepository,
+  tradeService: TradeService,
+  redisQueue: RedisQueue
+): Router {
+  const router = Router();
 
-export default router;
+  router.use(authMiddleware);
+  router.use(rateLimitMiddleware());
+
+  router.post('/', validate(CreateOrderSchema), asyncWrap(async (req: Request, res: Response) => {
+    const order = await orderIngestion.ingest(req.body, req.user!.userId);
+    await invalidateCache(`GET:/orders*`);
+
+    res.status(201).json({
+      success: true,
+      data: order,
+      timestamp: new Date().toISOString(),
+    });
+  }));
+
+  router.get('/', cacheMiddleware({ ttl: 30 }), asyncWrap(async (req: Request, res: Response) => {
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = Math.min(parseInt(req.query.limit as string) || 20, 100);
+
+    const { orders, total } = await orderRepo.findByUserId(req.user!.userId, page, limit);
+
+    res.json({
+      success: true,
+      data: orders,
+      page,
+      limit,
+      total,
+      timestamp: new Date().toISOString(),
+    });
+  }));
+
+  router.get('/:id', cacheMiddleware({ ttl: 30 }), asyncWrap(async (req: Request, res: Response) => {
+    const order = await orderRepo.findById(req.params.id);
